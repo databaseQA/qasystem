1dc5bbce7b0db0395dc983f1ec8a7fa23d1764a3
RainieWu <479086484@qq.com>
2016-11-08 10:35:21 +0800

    User/register页面

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -18,6 +18,7 @@ class UserController extends Controller {
     }
     
     public function register() {
+        $this->display();
         $data = array(
             "user_name" => $_POST['username'],
             "user_pwd" => $_POST['password'],
